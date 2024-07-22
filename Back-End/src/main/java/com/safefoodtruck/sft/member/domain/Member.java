bad2506a0c39f303c54cedfe7a53388955c520cc
junseok-99 <angec5645@naver.com>
2024-07-22 22:47:03 +0900

    feat: "Member Entity 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/member/domain/Member.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/member/domain/Member.java
@@ -4,20 +4,65 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "MEMBER")
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+@DynamicInsert
+@AllArgsConstructor
 @NoArgsConstructor
 public class Member {
 
     @Id
-    @Column(name = "member_email")
+    @NotNull
+    @Column(name = "email")
     private String email;
+
+    @NotNull
+    @Column(name = "password")
     String password;
+
+    @NotNull
+    @Column(name = "name", length = 50)
     String name;
+
+    @NotNull
+    @Column(name = "nickname", length = 50)
+    String nickname;
+
+    @NotNull
+    @Column(name = "gender")
+    int gender;
+
+    @NotNull
+    @Column(name = "birth", columnDefinition = "DATE")
+    LocalDate birth;
+
+    @NotNull
+    @Column(name = "phone_number", length = 50)
     String phoneNumber;
+
+    @Column(name = "business_number", length = 100)
+    @ColumnDefault("'not ceo'")
+    String businessNumber;
+
+    @Column(name = "role", length = 20)
+    @ColumnDefault("'customer'")
+    String role;
+
+    @Column(name = "reg_date", columnDefinition = "TIMESTAMP")
+    LocalDateTime regDate;
+
+    @Column(name = "is_resign")
+    @ColumnDefault("0")
+    int isResign;
 }
