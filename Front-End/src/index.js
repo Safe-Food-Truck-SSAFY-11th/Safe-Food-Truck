ccc3823601ba2d87022ce635743f4c3722e41cad
정다운 <dawoon221@naver.com>
2024-07-23 10:07:29 +0900

    feat: "프로젝트 설정 추가"
    - 라우터 링크
    - 폰트
    - 화면 사이즈

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
