ab73f17724a0edbeb9dcc8b4ed78faac749f9e87
prao <pch5521@gmail.com>
2024-07-28 23:56:41 +0900

    feat: "StoreService 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/store/service/StoreService.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/service/StoreService.java
@@ -1,5 +1,12 @@
 package com.safefoodtruck.sft.store.service;
 
+import com.safefoodtruck.sft.store.domain.Store;
+import com.safefoodtruck.sft.store.dto.request.StoreRegistRequestDto;
+import com.safefoodtruck.sft.store.dto.response.StoreInfoDto;
+
 public interface StoreService {
 
+	StoreInfoDto getStoreInfo(int storeId);
+	void registStore(StoreRegistRequestDto storeRegistRequestDto);
+	Store findStore(String email);
 }
--- Back-End/src/main/java/com/safefoodtruck/sft/store/service/StoreServiceImpl.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/service/StoreServiceImpl.java
@@ -1,5 +1,43 @@
 package com.safefoodtruck.sft.store.service;
 
+import org.springframework.stereotype.Service;
+
+import com.safefoodtruck.sft.member.domain.Member;
+import com.safefoodtruck.sft.member.repository.MemberRepository;
+import com.safefoodtruck.sft.store.domain.Store;
+import com.safefoodtruck.sft.store.dto.request.StoreRegistRequestDto;
+import com.safefoodtruck.sft.store.dto.response.StoreInfoDto;
+import com.safefoodtruck.sft.store.exception.StoreNotFoundException;
+import com.safefoodtruck.sft.store.repository.StoreRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class StoreServiceImpl implements StoreService {
 
+	private final StoreRepository storeRepository;
+	private final MemberRepository memberRepository;
+
+	@Override
+	public StoreInfoDto getStoreInfo(int storeId) {
+		Store store = storeRepository.findById(storeId)
+			.orElseThrow(StoreNotFoundException::new);
+		return StoreInfoDto.fromEntity(store);
+	}
+
+	@Override
+	public void registStore(StoreRegistRequestDto storeRegistRequestDto) {
+
+	}
+
+	@Override
+	public Store findStore(final String email) {
+		Member member = memberRepository.findByEmail(email);
+		return storeRepository.findByOwner(member).get();
+	}
 }
