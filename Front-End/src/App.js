d8daa9d51c628252d9dd3770ebb9ac87de767d8a
dawoon <52641434+wwhynott@users.noreply.github.com>
2024-07-28 11:46:44 +0900

    feat: "OwnerInfo 컴포넌트 개발"

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -10,6 +10,7 @@ import MainCustomer from './components/customer/mainPage/MainCustomer';
 import PermitAreaCheck from './components/owner/mainPage/PermitAreaCheck';
 import MyPageCustomer from './components/customer/myPage/MyPageCustomer';
 import CustomerUpdate from './components/customer/myPage/CustomerUpdate';
+import MyPageOwner from './components/owner/myPage/MyPageOwner';
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/permitAreaCheck" element={<PermitAreaCheck />} />
         <Route path="/mypage" element={<MyPageCustomer />} />
         <Route path="/customerUpdate" element={<CustomerUpdate />} />
+        <Route path="/mypageOwner" element={<MyPageOwner />} />
       </Routes>
     </div>
   );
---
+++ Front-End/src/components/owner/myPage/MyPageOwner.jsx
@@ -0,0 +1,13 @@
+import Header from '../../common/Header';
+import OwnerInfo from './OwnerInfo';
+
+const MyPageOwner = () => {
+    return (
+        <>
+            <Header />
+            <OwnerInfo />
+        </>
+    );
+};
+
+export default MyPageOwner;
