7be2afdbdd6b6b4d7d10ca07d63df86be96fd2ae
prao <pch5521@gmail.com>
2024-07-28 23:56:26 +0900

    feat: "StoreImage Entity 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/store/domain/StoreImage.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/domain/StoreImage.java
@@ -1,5 +1,46 @@
 package com.safefoodtruck.sft.store.domain;
 
+import org.hibernate.annotations.DynamicInsert;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Table(name = "store_image")
+@Entity
+@Getter
+@ToString
+@DynamicInsert
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreImage {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "store_image_id")
+	private long id;
+
+	@Column(name = "original_name")
+	private String originalName;
+
+	@Column(name = "saved_name")
+	private String savedName;
+
+	@Column(name = "saved_path")
+	private String savedPath;
+
+	@OneToOne(mappedBy = "storeImage")
+	private Store store;
+
+	public void addStore(Store store) {
+		this.store = store;
+	}
 }
