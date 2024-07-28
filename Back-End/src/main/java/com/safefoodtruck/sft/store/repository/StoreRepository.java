53e1e3e14be426891850d4441418ab9b4e5aba57
prao <pch5521@gmail.com>
2024-07-28 23:56:35 +0900

    feat: "StoreRepository 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/store/repository/StoreRepository.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/repository/StoreRepository.java
@@ -1,5 +1,14 @@
 package com.safefoodtruck.sft.store.repository;
 
-public interface StoreRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.safefoodtruck.sft.member.domain.Member;
+import com.safefoodtruck.sft.store.domain.Store;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Integer> {
+	Optional<Store> findByOwner(Member member);
 }
