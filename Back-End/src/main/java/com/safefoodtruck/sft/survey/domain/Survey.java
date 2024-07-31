15d4636cc08171082c341256b6ac7a0e75487828
장준석 <angec5689@naver.com>
2024-07-31 17:26:53 +0900

    feat: "Survey Entity 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/survey/domain/Survey.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/survey/domain/Survey.java
@@ -1,5 +1,52 @@
 package com.safefoodtruck.sft.survey.domain;
 
+import com.safefoodtruck.sft.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Table(name = "survey")
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString
 public class Survey {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "email")
+    @NotNull
+    private Member member;
+
+    @Column(name = "store_type")
+    private String storeType;
+
+    @Column(name = "dong")
+    private String dong;
+
+    @Column(name = "sigungu")
+    private String sigungu;
+
+    @Column(name = "latitue")
+    private String latitude;
+
+    @Column(name = "longitude")
+    private String longitude;
 }
