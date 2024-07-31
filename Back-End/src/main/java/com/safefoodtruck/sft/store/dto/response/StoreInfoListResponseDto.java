7539a303a7a19534a2b2b2adfab07dede1ea66b5
박창호 <pch5521@gmail.com>
2024-07-31 13:58:37 +0900

    feat: "영업 중인 모든 점포 조회 기능 추가"

---
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/dto/response/StoreInfoListResponseDto.java
@@ -0,0 +1,7 @@
+package com.safefoodtruck.sft.store.dto.response;
+
+import java.util.List;
+
+public record StoreInfoListResponseDto(List<StoreInfoResponseDto> storeInfoResponseDtos) {
+
+}
---
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/dto/response/StoreInfoResponseDto.java
@@ -0,0 +1,5 @@
+package com.safefoodtruck.sft.store.dto.response;
+
+public record StoreInfoResponseDto(int storeId, String title, String latitude, String longitude) {
+
+}
