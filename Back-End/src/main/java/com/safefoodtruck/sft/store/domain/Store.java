922b485265437cdf8a85d285ddf9a1018447925a
박창호 <pch5521@gmail.com>
2024-08-01 13:11:33 +0900

    refactor: "Store의 @Builder 제거"
    - [x] Postman test complete

--- Back-End/src/main/java/com/safefoodtruck/sft/store/domain/Store.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/domain/Store.java
@@ -20,16 +20,12 @@ import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.annotations.DynamicInsert;
 
 @Entity
-@Getter
 @Table(name = "store")
-@Builder
 @ToString
 @DynamicInsert
 @AllArgsConstructor
