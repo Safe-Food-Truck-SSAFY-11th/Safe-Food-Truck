adf5bf8e1d20ebb2b25d4b692aa0c139080ba5f8
prao <pch5521@gmail.com>
2024-08-04 20:15:37 +0900

    refactor: "메뉴 이미지 관련 dto 수정"

--- Back-End/src/main/java/com/safefoodtruck/sft/menu/dto/request/MenuRegistRequestDto.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/menu/dto/request/MenuRegistRequestDto.java
@@ -1,4 +1,6 @@
 package com.safefoodtruck.sft.menu.dto.request;
 
-public record MenuRegistRequestDto(String name, Integer price, String description) {
+import com.safefoodtruck.sft.menu.dto.MenuImageDto;
+
+public record MenuRegistRequestDto(String name, Integer price, String description, MenuImageDto menuImageDto) {
 }
--- Back-End/src/main/java/com/safefoodtruck/sft/menu/dto/request/MenuUpdateRequestDto.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/menu/dto/request/MenuUpdateRequestDto.java
@@ -1,5 +1,7 @@
 package com.safefoodtruck.sft.menu.dto.request;
 
-public record MenuUpdateRequestDto(String name, Integer price, String description) {
+import com.safefoodtruck.sft.menu.dto.MenuImageDto;
+
+public record MenuUpdateRequestDto(String name, Integer price, String description, MenuImageDto menuImageDto) {
 
 }
