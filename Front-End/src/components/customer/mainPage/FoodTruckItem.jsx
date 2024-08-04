dff01bfe5fc54902b29f744ff06887aa616c1a80
junhuk4079 <junhuk4079@gmail.com>
2024-08-04 20:20:28 +0900

    feat: "손님 메인페이지 푸드트럭 목록 확인 기능 추가"

--- Front-End/src/components/customer/mainPage/FoodTruckItem.jsx
+++ Front-End/src/components/customer/mainPage/FoodTruckItem.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import styles from './FoodTruckItem.module.css';
 
-function FoodTruckItem({ id , name, category, address, rating }) {
+function FoodTruckItem({ name, address , distance , onClick}) {
 
+  distance = parseFloat(distance).toFixed(1);
 
   return (
-    <div className={styles.foodTruckItem}>
+    <div className={styles.foodTruckItem} onClick={onClick}>
       <img src="/path/to/food-truck-icon.png" alt="Food Truck Icon" className={styles.icon} />
       <div className={styles.details}>
-        <h3>{name} <span className={styles.rating}>★ {rating}</span></h3>
-        <p className={styles.category}>{category}</p>
-        <p className={styles.address}>{address}</p>
+        <h3>{name} </h3>
+        {/* <p className={styles.category}>{category}</p> */}
+        <p className={styles.address}>현위치 : {address}</p>
+        <p className={styles.distance}>{distance}km 만큼 멀리 있어요!</p>
       </div>
     </div>
   );
