54fd242ece1fad303d17ee5fb416a135c756a65c
장준석 <angec5689@naver.com>
2024-08-04 16:22:28 +0900

    feat: "스케줄 조회 API 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/schedule/dto/ScheduleDto.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/schedule/dto/ScheduleDto.java
@@ -1,9 +1,12 @@
 package com.safefoodtruck.sft.schedule.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ScheduleDto {
+    private Integer scheduleId;
     private Integer storeId;
     private Integer day;
     private String address;
