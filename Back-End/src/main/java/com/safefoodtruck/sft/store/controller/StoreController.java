c108ff37b34285af44de8580d97d74f36f61d769
박창호 <pch5521@gmail.com>
2024-08-05 15:07:54 +0900

    refactor: "어노테이션 정리"

--- Back-End/src/main/java/com/safefoodtruck/sft/store/controller/StoreController.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/store/controller/StoreController.java
@@ -1,6 +1,5 @@
 package com.safefoodtruck.sft.store.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.safefoodtruck.sft.common.dto.ErrorResponseDto;
 import com.safefoodtruck.sft.menu.dto.response.MenuListResponseDto;
 import com.safefoodtruck.sft.store.dto.request.StoreLocationRequestDto;
@@ -25,7 +24,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequestMapping("/stores")
@@ -156,7 +163,7 @@ public class StoreController {
     }
 
     @ExceptionHandler({StoreNotFoundException.class, StoreImageNotFoundException.class})
-    public ResponseEntity<ErrorResponseDto> handleStoreException(Exception e) throws JsonProcessingException {
+    public ResponseEntity<ErrorResponseDto> handleStoreException(Exception e) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(
             HttpStatus.INTERNAL_SERVER_ERROR.value(),
             e.getMessage(),
