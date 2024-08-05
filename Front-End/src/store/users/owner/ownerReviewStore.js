875092f390fb19f0cfd2a71241dc39477955f53d
정다운 <dawoon221@naver.com>
2024-08-05 17:25:25 +0900

    feat: "리뷰 답글 등록 API 연결"

--- Front-End/src/store/users/owner/ownerReviewStore.js
+++ Front-End/src/store/users/owner/ownerReviewStore.js
@@ -2,6 +2,8 @@ import { create } from 'zustand';
 import axios from 'utils/axiosInstance';
 
 const useOwnerReviewStore = create(() => ({
+    
+    // 내 점포 리뷰 조회
     getReviewList: async (storeId) => {
         try {
             const response = await axios.get(`reviews/${storeId}`);
@@ -10,6 +12,16 @@ const useOwnerReviewStore = create(() => ({
             console.error("리뷰 조회 실패", error);
         }
     },
+
+    // 답글 등록
+    submitReply: async (replyData) => {
+        try {
+            const response = await axios.post('replies', replyData);
+            console.log(response.data);
+        } catch (error) {
+            console.error("답글 등록 실패: ", error)
+        }
+    }
 }));
 
 export default useOwnerReviewStore;
