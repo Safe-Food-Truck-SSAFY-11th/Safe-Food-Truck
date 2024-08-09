2d897e05e96ec9a73082d188ef3125491c3d27d7
junhuk4079 <junhuk4079@gmail.com>
2024-08-09 12:46:12 +0900

    refactor: "텍스트 표시 문구 변경"

--- Front-End/src/components/customer/myPage/OrderNow.jsx
+++ Front-End/src/components/customer/myPage/OrderNow.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './OrderNow.module.css';
+import useFoodTruckStore from 'store/trucks/useFoodTruckStore';
 
 const OrderNow = ({ memberInfo, nowOrder }) => {
 
-  // nowOrder가 없을 때
+// 푸드트럭 이름 가져오려고 스토어 호출
+const { getFoodTruck , selectedTruck } = useFoodTruckStore();
+
+// 어느 푸드트럭에서 음식 준비중인지 표시하려고 함수 호출
+useEffect(() => {
+  getFoodTruck(nowOrder?.storeId)
+},[])
+
+  // 현재 진행중인 주문이 없을 때
   if (!nowOrder) {
     return (
       <div className={styles.container}>
@@ -14,14 +23,14 @@ const OrderNow = ({ memberInfo, nowOrder }) => {
     );
   }
 
-  // Cooking status에 따른 메시지 출력
+  // 현재 주문 진행 상태에 맞춰서 텍스트 변동 시켜서 출력
   let statusMessage = '';
   switch (nowOrder.cookingStatus) {
     case 'waiting':
-      statusMessage = '매장에서 주문을 확인하고 있어요';
+      statusMessage = '주문을 확인하고 있어요';
       break;
     case 'preparing':
-      statusMessage = '메뉴를 준비중이에요';
+      statusMessage = '음식을 준비중이에요';
       break;
     case 'completed':
       statusMessage = '조리가 완료 됬어요!';
@@ -29,7 +38,7 @@ const OrderNow = ({ memberInfo, nowOrder }) => {
     default:
       statusMessage = '주문 상태를 확인할 수 없습니다.';
   }
-  
+
   const orderMenus = nowOrder.orderMenuListResponseDto.orderMenuResponseDtos;
   const menuCount = orderMenus.length;
   const menuText = menuCount > 1
@@ -43,7 +52,7 @@ const OrderNow = ({ memberInfo, nowOrder }) => {
         <span className={styles.orderTime}>주문 시간 : {nowOrder.orderTime}</span>
       </div>
       <div className={styles.orderDetails}>
-        <p>{menuText}</p>
+        <p>{selectedTruck.name}에서 {menuText} 주문 했어요</p>
       </div>
     </div>
   );
