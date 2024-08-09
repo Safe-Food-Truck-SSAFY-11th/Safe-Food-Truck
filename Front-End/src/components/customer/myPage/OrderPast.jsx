4eaaed18c11e053c86f9b8839ddb8d0ff3bc1817
junhuk4079 <junhuk4079@gmail.com>
2024-08-09 12:30:38 +0900

    refactor: "과거 주문내역 여러건이면 총액이랑 메뉴 외 x건으로 표시하게 로직 변경"

--- Front-End/src/components/customer/myPage/OrderPast.jsx
+++ Front-End/src/components/customer/myPage/OrderPast.jsx
@@ -5,37 +5,32 @@ import styles from './OrderPast.module.css';
 const OrderPast = ({ memberInfo, pastOrders }) => {
   const navigate = useNavigate();
 
+  // 과거 주문 내역을 역순으로 정렬
   const results = [...pastOrders.customerOrderResponseDtos].reverse();
 
   const handleReviewButtonClick = (orderId) => {
     navigate(`/createReview/${orderId}`, { state: { memberInfo } });
   };
-
   return (
     <div className={styles.container}>
       {results.length > 0 ? (
         <>
           <h3>{memberInfo.nickname} 🖐 님이 구매했던 내역이에요!</h3>
-          {results.map(order => (
+          {results.map((order) => (
             <div key={order.orderId} className={styles.orderCard}>
               <div className={styles.orderContent}>
                 <div className={styles.orderDetails}>
                   <h4>{order.storeName}</h4>
-                  {order.menuResponseDtos.map(menu => (
-                    <div key={menu.menuId} className={styles.menuDetails}>
-                      {menu.menuImageDto ? (
-                        <img
-                          src={menu.menuImageDto.savedUrl}
-                          alt={`Menu Image ${menu.menuId}`}
-                          className={styles.menuImage}
-                        />
-                      ) : (
-                        <p>사진</p>
-                      )}
-                      <p>{menu.name} {order.amount / menu.price} 개 </p>
-                      <p>{order.amount} 원</p>
-                    </div>
-                  ))}
+                  <p>
+                    {order.menuResponseDtos && order.menuResponseDtos.length > 0 ? (
+                      order.menuResponseDtos.length === 1
+                        ? `${order.menuResponseDtos[0].name} ${order.amount / order.menuResponseDtos[0].price}개` // 메뉴가 1개인 경우
+                        : `${order.menuResponseDtos[0].name} 외 ${order.menuResponseDtos.length - 1}건` // 메뉴가 2개 이상인 경우
+                    ) : (
+                      '메뉴 정보가 없습니다' // menuResponseDtos가 비어있는 경우
+                    )}
+                  </p>
+                  <p>{order.amount} 원</p>
                 </div>
                 <button
                   className={styles.reviewButton}
