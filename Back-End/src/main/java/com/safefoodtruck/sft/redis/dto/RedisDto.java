c83df6450cf21dbef6e6a9a2aaabe58543690be3
prao <pch5521@gmail.com>
2024-08-10 22:39:14 +0900

    refactor: "RedisDto 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/redis/dto/RedisDto.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/redis/dto/RedisDto.java
@@ -1,19 +1,7 @@
 package com.safefoodtruck.sft.redis.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
 @Builder
-public class RedisDto {
-	private String key;
-	private String value;
+public record RedisDto(String key, String value) {
 }
