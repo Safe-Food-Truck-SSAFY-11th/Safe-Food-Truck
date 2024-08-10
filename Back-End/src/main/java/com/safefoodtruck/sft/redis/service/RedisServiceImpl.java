a4ef9022c3d2ce17ef68ceda8101a6be5733727f
prao <pch5521@gmail.com>
2024-08-10 22:48:31 +0900

    feat: "방송 여부 redis에 저장 기능 추가"

--- Back-End/src/main/java/com/safefoodtruck/sft/redis/service/RedisServiceImpl.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/redis/service/RedisServiceImpl.java
@@ -1,7 +1,6 @@
 package com.safefoodtruck.sft.redis.service;
 
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.safefoodtruck.sft.redis.dto.RedisDto;
@@ -14,20 +13,19 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class RedisServiceImpl implements RedisService {
 
-	private final StringRedisTemplate stringRedisTemplate;
 	private final RedisTemplate<String, Object> redisTemplate;
 
 	@Override
 	public void setValue(RedisDto redisDto) {
-		String key = redisDto.getKey();
-		Object value = redisDto.getValue();
+		String key = redisDto.key();
+		Object value = redisDto.value();
 		log.info("Setting value for key: {}", key);
 		redisTemplate.opsForValue().set(key, value);
 	}
 
 	@Override
 	public String getValue(RedisDto redisDto) {
-		String key = redisDto.getKey();
+		String key = redisDto.key();
 		log.info("Getting value for key: {}", key);
 		Object value = redisTemplate.opsForValue().get(key);
 		return value != null ? value.toString() : null;
@@ -35,7 +33,7 @@ public class RedisServiceImpl implements RedisService {
 
 	@Override
 	public void deleteValue(RedisDto redisDto) {
-		String key = redisDto.getKey();
+		String key = redisDto.key();
 		log.info("Deleting value for key: {}", key);
 		redisTemplate.delete(key);
 	}
