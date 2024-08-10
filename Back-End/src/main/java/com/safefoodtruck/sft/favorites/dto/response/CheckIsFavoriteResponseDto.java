605e0dee4684f84527a1d6ef54f803a9ae670d16
조용훈 <sjsin0905@naver.com>
2024-08-10 22:42:13 +0900

    feat: "특정 사용자로 가게 찜 여부 조회"

---
+++ Back-End/src/main/java/com/safefoodtruck/sft/favorites/dto/response/CheckIsFavoriteResponseDto.java
@@ -0,0 +1,10 @@
+package com.safefoodtruck.sft.favorites.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CheckIsFavoriteResponseDto {
+    private Integer favoriteId;
+}
