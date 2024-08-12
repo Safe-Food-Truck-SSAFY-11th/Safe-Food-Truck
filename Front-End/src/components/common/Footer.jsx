d66bd71595c87607a4b0be8013e601023b3f2f9c
이윤주 <114348883+yujachaa@users.noreply.github.com>
2024-08-13 02:00:02 +0900

    design: "헤더, 푸터 아이콘, css 수정"

--- Front-End/src/components/common/Footer.jsx
+++ Front-End/src/components/common/Footer.jsx
@@ -1,61 +1,144 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
+import Notification from "components/customer/mainPage/Notification";
+import { TbHome, TbMessage2Star } from "react-icons/tb";
+import { IoNotificationsOutline, IoCartOutline } from "react-icons/io5";
+import { GoPerson } from "react-icons/go";
+import { PiListStar } from "react-icons/pi";
+import { LuSettings } from "react-icons/lu";
+import { RxPerson } from "react-icons/rx";
+import { BsPerson } from "react-icons/bs";
+import { BsCart4 } from "react-icons/bs";
+import { PiShoppingCartBold } from "react-icons/pi";
+import { TbBell } from "react-icons/tb";
+import { BiBell } from "react-icons/bi";
 
 const Footer = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [role, setRole] = useState(sessionStorage.getItem("role"));
+  const [showNotification, setNotification] = useState(false);
+  const [activeIcon, setActiveIcon] = useState(""); // 현재 클릭된 아이콘을 저장
+
+  const handleNotificationClick = () => {
+    setNotification(!showNotification);
+    setActiveIcon("notification");
+  };
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
+    if (activeIcon === "settings") {
+      setActiveIcon("");
+    } else {
+      setActiveIcon("settings");
+    }
+  };
+
+  const handleIconClick = (icon) => {
+    setActiveIcon(icon);
   };
 
   if (role.indexOf("customer") !== -1) {
     return (
-      <footer className={styles.footer}>
-        <Link to="/mainCustomer" className={styles.menuItem}>
-          <i className={`${styles.icon} ${styles.homeIcon}`}></i>
-          <span>홈</span>
-        </Link>
-        <Link to="/mypageCustomer" className={styles.menuItem}>
-          <i className={`${styles.icon} ${styles.userIcon}`}></i>
-          <span>마이페이지</span>
-        </Link>
-        <Link to="/cart" className={styles.menuItem}>
-          <i className={`${styles.icon} ${styles.cartIcon}`}></i>
-          <span>장바구니</span>
-        </Link>
-        <Link
-          to={{ pathname: "/mypageCustomer", state: { fromFooter: true } }}
-          className={styles.menuItem}
-        >
-          <i className={`${styles.icon} ${styles.heartIcon}`}></i>
-          <span>찜목록</span>
-        </Link>
-      </footer>
+      <>
+        <footer className={styles.footer}>
+          <Link
+            to="/mainCustomer"
+            className={styles.menuItem}
+            onClick={() => handleIconClick("home")}
+          >
+            <TbHome
+              size="35"
+              color={activeIcon === "home" ? "#ffcc66" : "black"}
+            />
+          </Link>
+          <div className={styles.menuItem} onClick={handleNotificationClick}>
+            <BiBell
+              size="36"
+              color={activeIcon === "notification" ? "#ffcc66" : "black"}
+            />
+          </div>
+          <Link
+            to="/cart"
+            className={styles.menuItem}
+            onClick={() => handleIconClick("cart")}
+          >
+            <PiShoppingCartBold
+              size="34"
+              color={activeIcon === "cart" ? "#ffcc66" : "black"}
+            />
+          </Link>
+          <Link
+            to="/mypageCustomer"
+            className={styles.menuItem}
+            onClick={() => handleIconClick("mypage")}
+          >
+            <RxPerson
+              size="34"
+              color={activeIcon === "mypage" ? "#ffcc66" : "black"}
+              strokeWidth="0.4"
+            />
+          </Link>
+        </footer>
+        {showNotification ? (
+          <Notification
+            show={showNotification}
+            onClose={() => {
+              setNotification(false);
+              setActiveIcon("");
+            }}
+          />
+        ) : null}
+      </>
     );
   } else if (role.indexOf("owner") !== -1) {
     return (
       <footer className={styles.footer}>
-        <Link to="/mainOwner" className={styles.menuItem}>
-          <i className={`${styles.icon} ${styles.homeIcon}`}></i>
-          <span>홈</span>
+        <Link
+          to="/mainOwner"
+          className={styles.menuItem}
+          onClick={() => handleIconClick("home")}
+        >
+          <TbHome
+            size="35"
+            color={activeIcon === "home" ? "#e2eed0" : "black"}
+          />
         </Link>
-        <Link to="/mypageOwner" className={styles.menuItem}>
-          <i className={`${styles.icon} ${styles.userIcon}`}></i>
-          <span>마이페이지</span>
+        <Link
+          to="/mypageOwner"
+          className={styles.menuItem}
+          onClick={() => handleIconClick("mypage")}
+        >
+          <RxPerson
+            size="34"
+            color={activeIcon === "mypage" ? "#e2eed0" : "black"}
+            strokeWidth="0.4"
+          />
         </Link>
-        <Link to="/ownerReview" className={styles.menuItem}>
-          <i className={`${styles.icon} ${styles.reviewIcon}`}></i>
-          <span>리뷰관리</span>
+        <Link
+          to="/ownerReview"
+          className={styles.menuItem}
+          onClick={() => handleIconClick("review")}
+        >
+          <TbMessage2Star
+            size="35"
+            color={activeIcon === "review" ? "#e2eed0" : "black"}
+          />
         </Link>
-        <div className={styles.menuItem} onClick={toggleDropdown}>
-          <i className={`${styles.icon} ${styles.settingsIcon}`}></i>
-          <span>트럭관리</span>
+        <div
+          className={styles.menuItem}
+          onClick={() => {
+            toggleDropdown();
+          }}
+        >
+          <LuSettings
+            size="35"
+            color={activeIcon === "settings" ? "#e2eed0" : "black"}
+          />
           {isDropdownOpen && (
             <div className={styles.dropdown}>
-              <Link to="/manageTruck">점포정보 수정</Link>
-              <Link to="/manageSchedule">스케줄 관리</Link>
+              <Link to="/manageTruck">트럭정보수정</Link>
+              <Link to="/manageSchedule">스케줄관리</Link>
               <Link to="/manageMenu">메뉴관리</Link>
             </div>
           )}
