47f6732d8aa1f34faac9a4fb0fc6023d6273c1ef
junhuk4079 <junhuk4079@gmail.com>
2024-08-13 09:33:26 +0900

    refactor: "마이페이지 customerInfo css 수정!"

--- Front-End/src/components/customer/myPage/CustomerInfo.jsx
+++ Front-End/src/components/customer/myPage/CustomerInfo.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './CustomerInfo.module.css';
-import profile_img from "assets/images/profile_image.png"
+import profile_img from "assets/images/profile_image.png";
 
-const CustomerInfo = ({ onSelect, activeButton, memberInfo ,pastOrders  }) => {
-
-
-
+const CustomerInfo = ({ onSelect, activeButton, memberInfo, pastOrders }) => {
   const navigate = useNavigate();
 
   const handleProfileEdit = () => {
@@ -19,21 +16,23 @@ const CustomerInfo = ({ onSelect, activeButton, memberInfo ,pastOrders  }) => {
     return <div>로딩중입니다!!</div>;
   }
 
-    // 과거 주문 목록을 토대로 내가 푸드트럭 몇 번 이용 했는지 횟수 가져옴!
+  // 과거 주문 목록을 토대로 내가 푸드트럭 몇 번 이용했는지 횟수 가져옴!
   const pastOrderNum = pastOrders?.customerOrderResponseDtos?.length;
 
   return (
     <div className={styles.container}>
-      <div className={styles.profileSection}>
-        <img src={imageUrl} alt="Profile" className={styles.profileImage} /> 
-        <button className={styles.profileButton} onClick={handleProfileEdit}>
-          내 정보 수정
-        </button>
-      </div>
-      <div className={styles.infoSection}>
-        <h2>반갑습니다 {memberInfo.nickname}님!</h2>
-        <p>오늘 푸드트럭 어때요?</p>
-        <p>지금까지 푸드트럭 {pastOrderNum}번 만났어요!</p>
+      <div className={styles.contentWrapper}>
+        <div className={styles.profileSection}>
+          <img src={imageUrl} alt="Profile" className={styles.profileImage} />
+          <button className={styles.profileButton} onClick={handleProfileEdit}>
+            내 정보 수정
+          </button>
+        </div>
+        <div className={styles.infoSection}>
+          <h2>반갑습니다 {memberInfo.nickname}님!</h2>
+          <p>오늘 푸드트럭 어때요?</p>
+          <p>지금까지 푸드트럭 {pastOrderNum}번 만났어요!</p>
+        </div>
         <div className={styles.buttons}>
           <button
             className={`${styles.actionButton} ${activeButton === 'liked' ? styles.active : ''}`}
