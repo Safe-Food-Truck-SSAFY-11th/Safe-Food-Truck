2bbd9bc8295ae029d129cb70f387df71d288c62f
장준석 <angec5689@naver.com>
2024-08-15 21:29:09 +0900

    refactor: "다중 연결 해제"

--- Back-End/src/main/java/com/safefoodtruck/sft/globalnotification/repository/EmitterRepository.java
+++ Back-End/src/main/java/com/safefoodtruck/sft/globalnotification/repository/EmitterRepository.java
@@ -1,7 +1,5 @@
 package com.safefoodtruck.sft.globalnotification.repository;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import lombok.RequiredArgsConstructor;
@@ -12,30 +10,17 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequiredArgsConstructor
 public class EmitterRepository {
 
-    private final Map<String, List<SseEmitter>> emitters = new ConcurrentHashMap<>();
+    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
 
     public void save(String email, SseEmitter emitter) {
-        if (!emitters.containsKey(email)) {
-            emitters.put(email, new ArrayList<>());
-        }
-        emitters.get(email).add(emitter);
+        emitters.put(email, emitter);
     }
 
-    public void delete(String email, SseEmitter emitterToRemove) {
-        if (!emitters.containsKey(email) || emitters.get(email).isEmpty()) {
-            return;
-        }
-
-        List<SseEmitter> emitterList = emitters.get(email);
-        if (emitterList != null) {
-            emitterList.remove(emitterToRemove); // 특정 SseEmitter 삭제
-            if (emitterList.isEmpty()) {
-                emitters.remove(email); // 리스트가 비면 map에서 키 삭제
-            }
-        }
+    public void deleteById(String email) {
+        emitters.remove(email);
     }
 
-    public List<SseEmitter> get(String email) {
+    public SseEmitter get(String email) {
         return emitters.get(email);
     }
 }
